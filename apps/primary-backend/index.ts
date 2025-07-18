e7350352db7b58f2cd53125f44deff98b0f17038
StackSamuraiK <kshitizsrivastav3344@gmail.com>
2025-07-19 00:10:23 +0530

    added-prompts-table

--- apps/primary-backend/index.ts
+++ apps/primary-backend/index.ts
@@ -1,1 +1,15 @@
 import { prismaClient } from "db/client";
+import express from "express";
+import { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/project' , async(req: Request , res: Response)=>{
+    
+})
+
+app.listen(3000);
